76454874c4e88155b918aa06463748d472f45163
Slathouwers <sander.lathouwers@live.be>
2021-09-18 18:30:05 +0000

    tested simple printing of string without '%'

--- misc/main.c
+++ misc/main.c
@@ -6,7 +6,7 @@
 /*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/17 10:41:02 by slathouw          #+#    #+#             */
-/*   Updated: 2021/09/17 12:24:09 by slathouw         ###   ########.fr       */
+/*   Updated: 2021/09/18 10:38:28 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,7 @@
 
 int	main(void)
 {
-	printf("%.2md",10);
-	//ft_printf("blabla\n%s\nblablabla\n%i\nblablabla\n%c\n", "Sander", 10, 'X');
-
+	//printf("%.10bd",10);
+	ft_printf("blablaa;lskdjf;alksjdf;lkajsdfoijeiijnefjknsdinvsinvivndjisvnfnsifnsjndfkjs", "Sander", 10, 'X');
 	return (0);
 }
