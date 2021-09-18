fb24a79b6de8257a5e0767e0ce19cffb9f5f2192
Slathouwers <sander.lathouwers@live.be>
2021-09-18 18:29:12 +0000

    format struct added to header

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/17 10:12:18 by slathouw          #+#    #+#             */
-/*   Updated: 2021/09/17 10:40:03 by slathouw         ###   ########.fr       */
+/*   Updated: 2021/09/18 10:36:33 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,16 @@
 # include <stdlib.h>
 # include <stdarg.h>
 # include "libft.h"
+
+typedef struct s_format
+{
+	const char	*fstr;
+	int			num_printed;
+	int			left_align;
+	int			zero;
+	int			min_width;
+	int			precision;
+}				t_format;
 
 int	ft_printf(const char *format, ...);
 
