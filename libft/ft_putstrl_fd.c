71802f2b1e502fe1e76e6339be99b4fa379b604b
Slathouwers <sander.lathouwers@live.be>
2021-09-18 22:23:43 +0000

    corrected norm errors in ft_putstrl_fd

--- libft/ft_putstrl_fd.c
+++ libft/ft_putstrl_fd.c
@@ -6,18 +6,18 @@
 /*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/18 18:51:05 by slathouw          #+#    #+#             */
-/*   Updated: 2021/09/18 22:03:21 by slathouw         ###   ########.fr       */
+/*   Updated: 2021/09/18 22:22:40 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_putstrn_fd(char const *s, size_t n, int fd)
+size_t	ft_putstrl_fd(char const *s, size_t n, int fd)
 {
 	size_t	len;
 
 	if (!s || fd < 0)
-		return ;
+		return (0);
 	len = ft_strlen(s);
 	if (len < n)
 		n = len;
