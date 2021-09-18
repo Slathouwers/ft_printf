7eb80959cfd6bdb16bba0773f9de5bba854c2fea
Slathouwers <sander.lathouwers@live.be>
2021-09-18 21:51:01 +0000

    safety against surpassing \0 termination

--- libft/ft_putstrn_fd.c
+++ libft/ft_putstrn_fd.c
@@ -6,7 +6,7 @@
 /*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/18 18:51:05 by slathouw          #+#    #+#             */
-/*   Updated: 2021/09/18 21:33:30 by slathouw         ###   ########.fr       */
+/*   Updated: 2021/09/18 21:50:27 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,12 @@
 
 void	ft_putstrn_fd(char const *s, size_t n, int fd)
 {
+	size_t	len;
+
 	if (!s || fd < 0)
 		return ;
+	len = ft_strlen(s);
+	if (len < n)
+		n = len;
 	write(fd, s, n);
 }
