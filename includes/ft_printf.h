ce8f414f4ee492391a27d9eaeb1e3692fff9f341
Slathouwers <sander.lathouwers@live.be>
2021-09-19 15:45:37 +0000

    fixed libft header include

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/17 10:12:18 by slathouw          #+#    #+#             */
-/*   Updated: 2021/09/18 10:36:33 by slathouw         ###   ########.fr       */
+/*   Updated: 2021/09/19 15:43:58 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # include <stdlib.h>
 # include <stdarg.h>
-# include "libft.h"
+# include "../libft/libft.h"
 
 typedef struct s_format
 {
