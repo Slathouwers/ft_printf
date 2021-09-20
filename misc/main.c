b9cbd29a27133a1760d55b9afd4019a5b2bd9234
Slathouwers <sander.lathouwers@live.be>
2021-09-20 08:30:21 +0000

    added tests for nex functions

--- misc/main.c
+++ misc/main.c
@@ -6,7 +6,7 @@
 /*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/17 10:41:02 by slathouw          #+#    #+#             */
-/*   Updated: 2021/09/19 16:03:04 by slathouw         ###   ########.fr       */
+/*   Updated: 2021/09/20 08:03:20 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,9 @@ int	main(void)
 {
 	int n = -2147483648;
 	unsigned int un = 4294967295;
+	char str[] = "SANDERB";
 
+	/* -------------- Unsigned int
 	char *strun = ft_uitoa(un);
 	//printf("%.10bd",10);
 	printf("%i\n",ft_printf("%%%s%i%u","Sander", 50, un));
@@ -26,6 +28,11 @@ int	main(void)
 	printf("\nun = %s\n", strun);
 	ft_putunbr_fd(un, 1);
 	ft_putchar_fd('\n', 1);
-	free(strun);
+	free(strun); */
+	/*-----------HEX-------*/
+	/* ft_strrev(str);
+	printf("%s", str); */
+	//ft_putlong_base_fd(9223372036854775807, "0123456789ABCDEF", 1);
+	printf("x = %x\n X = %X", -99, -99);
 	return (0);
 }
