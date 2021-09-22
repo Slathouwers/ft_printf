e2aa05bd645bf28b01a7db0461ed069519b1098c
Slathouwers <sander.lathouwers@live.be>
2021-09-22 13:13:49 +0000

    bugfix ft_strldup

--- libft/ft_strldup.c
+++ libft/ft_strldup.c
@@ -6,7 +6,7 @@
 /*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/22 10:26:30 by slathouw          #+#    #+#             */
-/*   Updated: 2021/09/22 10:38:49 by slathouw         ###   ########.fr       */
+/*   Updated: 2021/09/22 12:57:10 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,18 @@
 
 char	*ft_strldup(const char *s, size_t len)
 {
-	size_t	size;
-	char	*dup;
+	size_t		size;
+	const char	*s_start;
+	char		*dup;
 
 	size = 0;
+	s_start = s;
 	if (!s)
 		return (NULL);
-	while (*s && len--)
+	while (*s++ && len--)
 		size++;
 	dup = (char *) malloc(sizeof(char) * (size + 1));
-	ft_memmove(dup, s, size);
+	ft_memmove(dup, s_start, size);
 	dup[size] = '\0';
 	return (dup);
 }
