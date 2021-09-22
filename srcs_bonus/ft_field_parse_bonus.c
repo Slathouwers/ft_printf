fcedd9ce315529003696c9d4dd1904eddb96b0ea
Slathouwers <sander.lathouwers@live.be>
2021-09-22 13:14:15 +0000

    added ' ' and + flags to parser

--- srcs_bonus/ft_field_parse_bonus.c
+++ srcs_bonus/ft_field_parse_bonus.c
@@ -6,7 +6,7 @@
 /*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/22 08:29:07 by slathouw          #+#    #+#             */
-/*   Updated: 2021/09/22 08:33:40 by slathouw         ###   ########.fr       */
+/*   Updated: 2021/09/22 12:56:26 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ void	ft_field_parse(t_field *fld, char *pcnt_ptr)
 	fld->percent_ptr = pcnt_ptr;
 	fld->parse_ptr = pcnt_ptr + 1;
 	fld->precision = -1;
-	while (ft_strchr("-0#", *fld->parse_ptr))
+	while (ft_strchr("-0# +", *fld->parse_ptr))
 	{
 		if (*fld->parse_ptr == '-')
 			fld->left_align = 1;
@@ -49,6 +49,10 @@ void	ft_field_parse(t_field *fld, char *pcnt_ptr)
 			fld->zero_padding = 1;
 		if (*fld->parse_ptr == '#')
 			fld->alt_form = 1;
+		if (*fld->parse_ptr == ' ')
+			fld->space = 1;
+		if (*fld->parse_ptr == '+')
+			fld->plus_sign = 1;
 		fld->parse_ptr++;
 	}
 	if (ft_isdigit(*fld->parse_ptr))
