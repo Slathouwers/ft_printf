8463f9fe1fdf547065ee8ff780afdfdd9b0dcbfd
Slathouwers <sander.lathouwers@live.be>
2021-09-22 09:33:14 +0000

    %c with field flags implemented

--- srcs_bonus/ft_print_char_bonus.c
+++ srcs_bonus/ft_print_char_bonus.c
@@ -6,19 +6,41 @@
 /*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/21 10:08:45 by slathouw          #+#    #+#             */
-/*   Updated: 2021/09/22 08:11:33 by slathouw         ###   ########.fr       */
+/*   Updated: 2021/09/22 08:57:36 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf_bonus.h"
 
-void	ft_print_char(t_field *fld, t_format *fmt, va_list ap)
+static void	print_c(t_field *fld, t_format *fmt, va_list ap)
 {
-	(void) fld;
-	if (*fmt->fstr == '%')
+	if (*fld->parse_ptr == '%')
 		ft_putchar_fd('%', 1);
 	else
 		ft_putchar_fd((char) va_arg(ap, int), 1);
 	fmt->num_printed++;
+}
+
+static void	print_width(t_field *fld, t_format *fmt, char c)
+{
+	ft_putchar_fd(c, 1);
+	fmt->num_printed++;
+	fld->min_width--;
+}
+
+void	ft_print_char(t_field *fld, t_format *fmt, va_list ap)
+{
+	char	padding;
+
+	if ((fld->zero_padding) && (*fld->parse_ptr == '%') && !(fld->left_align))
+		padding = '0';
+	else
+		padding = ' ';
+	if (fld->left_align)
+		print_c(fld, fmt, ap);
+	while (fld->min_width > 1)
+		print_width(fld, fmt, padding);
+	if (!(fld->left_align))
+		print_c(fld, fmt, ap);
 	fmt->fstr = fld->parse_ptr + 1;
 }
